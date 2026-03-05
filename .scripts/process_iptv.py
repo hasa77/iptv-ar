55fdcf10a8137ee3063e079e6a06c129d6f88afa
hasa77 <32495604+hasa77@users.noreply.github.com>
2026-03-05 13:06:00 +0000

    Update process_iptv.py

--- .scripts/process_iptv.py
+++ .scripts/process_iptv.py
@@ -16,7 +16,7 @@ EPG_SOURCES = [
     "https://epgshare01.online/epgshare01/epg_ripper_BEIN1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_EG1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_SA1.xml.gz",
-    "https://epgshare01.online/epgshare01/epg_ripper_SA2.xml.gz",,
+    "https://epgshare01.online/epgshare01/epg_ripper_SA2.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_US2.xml.gz",
     "https://iptv-epg.org/files/epg-meyqso.xml"
